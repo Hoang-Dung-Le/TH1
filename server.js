49706d86e724be384d862eaafcdf4178bfc6c3ce
Le Hoang Dung <hoangdungle36@gmail.com>
2023-10-05 07:56:21 +0700

    +TH2

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const app = require("./app");
 const config = require("./app/config");
 const MongoDB = require("./app/utils/mongodb.util");
 
-async function startServer () {
+// buoi 2
+
+async function startServer() {
   try {
     await MongoDB.connect(config.db.uri);
     console.log("Connected to the database!");
